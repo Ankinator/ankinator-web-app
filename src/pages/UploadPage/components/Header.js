2d5252c78e2f8f7fe58e1da02cfb5fba320bb512
Tim Bleicher <tim.bleicher@sap.com>
2023-03-10 16:38:09 +0100

    AN-20: better design

--- src/pages/UploadPage/components/Header.js
+++ src/pages/UploadPage/components/Header.js
@@ -5,15 +5,16 @@ import { bgColors } from '../../../App';
 
 function Header() {
     return (
-        <Navbar style={{ backgroundColor: bgColors.AltHint }} sticky="top" expand="lg">
+        <Navbar style={{ background: 'linear-gradient(to left, rgba(42, 152, 225, 0.7), rgba(167, 213, 243, 0.7))' }} sticky="top" expand="lg">
             <Container style={{ maxWidth: "100%", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-                <Navbar.Brand href="#Home" style={{ color: bgColors.Primär, fontSize: "xx-large", textDecoration: 'none' }}>
-                    <img
-                        alt="VR Stage Planner Logo"
-                        src={Image}
-                        width="50"
-                        height="50"
-                    />
+                <img
+                    alt="VR Stage Planner Logo"
+                    src={Image}
+                    width="50"
+                    height="50"
+                />
+                <Navbar.Brand href="#Home" style={{ color: bgColors.Text, fontSize: "xx-large", textDecoration: 'none' }}>
+
                     Ankinator
                 </Navbar.Brand>
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
