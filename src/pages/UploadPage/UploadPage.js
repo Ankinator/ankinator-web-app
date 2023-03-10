6cbd7f19a9576ea3d3b8d1e23d3f2484d8b2b8e7
Tim Bleicher <tim.bleicher@sap.com>
2023-03-10 13:37:36 +0100

    Design and settings

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import './UploadPage.css';
 import UploadPdfForm from './components/UploadPdfForm';
 import PdfSelector from './components/PdfSelector';
+import { bgColors } from '../../App';
+import { Container } from 'react-bootstrap';
 
 const UploadPage = () => {
   const [pdfFile, setPdfFile] = useState(null);
@@ -10,11 +13,11 @@ const UploadPage = () => {
   };
 
   return (
-    <div>
+    <Container style={{maxWidth: "100%", padding: 0, height: "100%", backgroundColor: bgColors.Hint}}>
       <h1>PDF Viewer App</h1>
       <UploadPdfForm onPdfSelect={handlePdfSelect} />
       <PdfSelector pdfFile={pdfFile} />
-    </div>
+    </Container>
   );
 };
 
