3a57ae9b24534035b2b5c06feb49963bfe038ee0
timbleicher <66074690+TimBleicher@users.noreply.github.com>
2023-03-13 10:52:51 +0100

    An 19 pdf uploader (#5)
    Co-authored-by: Tim Bleicher <tim.bleicher@sap.com>

--- src/pages/UploadPage/components/UploadPdfForm.js
+++ src/pages/UploadPage/components/UploadPdfForm.js
@@ -1,24 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Form, Button } from "react-bootstrap";
+import './UploadPdfForm.css';
 
 const UploadPdfForm = ({ onPdfSelect }) => {
   const [pdfFile, setPdfFile] = useState(null);
+  const [selectedName, setSelectedName] = useState("")
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
 
   const handlePdfSelect = (event) => {
     const selectedFile = event.target.files[0];
     setPdfFile(selectedFile);
     onPdfSelect(selectedFile);
+    setSelectedName(selectedFile.name);
   };
 
   return (
-    <div className="UploadPdfForm">
-    <h2>Skript hochladen (PDF)</h2>
-    <Form>
-      <Form.Group controlId="formFile">
-        <Form.Control type="file" accept="application/pdf" onChange={handlePdfSelect} />
-      </Form.Group>
-    </Form>
-  </div>
+    <Container className="app" style={{margin: 'auto', marginTop: '10px'}}>
+      <Row className="file-upload">
+        <Col style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <input type="file" ref={fileInputRef} style={{ display: 'none' }} onChange={handlePdfSelect} />
+          <Button type="button" onClick={handleButtonClick}>Upload script</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p style={{ textAlign: "center" }}>{selectedName || "No file selected"}</p>
+        </Col>
+      </Row>
+    </Container>
+
   );
 };
 
