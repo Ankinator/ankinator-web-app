7314a18530cec4bf74a5c106f508e54181ce8fc5
Tim Bleicher <tim.bleicher@sap.com>
2023-05-17 18:14:08 +0200

    API fix

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,8 @@ import Cookie from 'universal-cookie';
 const API_URL = 'http://localhost';
 
 export const sessionLogin = async () => {
-  var cookieAcc = new Cookie();
-  if (cookieAcc.get("access_token") === undefined) {
+  var cookie = new Cookie();
+  if (cookie.get("access_token") === undefined) {
     try {
       const response = await axios.post(`${API_URL}/session`, {
         headers: {
@@ -13,12 +13,14 @@ export const sessionLogin = async () => {
         }
       });
       var result = await response.data;
-      cookieAcc.set('access_token', result.access_token, { path: '/' });
+      cookie.set('access_token', result.access_token, { path: '/' });
+      getUser();
       return await response.data;
     } catch (error) {
       console.error(error);
     }
   } else {
+    getUser();
     return;
   }
 };
@@ -26,21 +28,49 @@ export const sessionLogin = async () => {
 export const postSelectedPages = async (pdfFile, selectedPages, subject, domain) => {
   var cookie = new Cookie();
   try {
+    var test = [];
+    
     const formData = new FormData();
-    formData.append('pdf', pdfFile, pdfFile.name);
-    /*formData.append('selectedPages', JSON.stringify(selectedPages));
-    formData.append('subject', subject);
-    formData.append('domain', domain);*/
-    const response = await axios.post(`${API_URL}/uploadpdf`, {
+    formData.append('file', pdfFile);
+    formData.append('pages', selectedPages);
+    formData.append('models', test);
+    formData.append('domain', "domain");
+    const response = await axios.post(`${API_URL}/uploadpdf`,formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
         'Authorization': 'Bearer ' + cookie.get('access_token')
-      },
-      body: formData
+      }
     });
     
     return await response.data;
   } catch (error) {
     console.error(error);
+  }
+};
+
+export const getUser = async () => {
+  var cookie = new Cookie();
+  try {
+    const response = await axios.get(`${API_URL}/users/me`, {
+      headers: {
+        'Authorization': 'Bearer ' + cookie.get('access_token')
+      }
+    });
+    console.log(response.data);
+    return await response.data;
+  } catch (error) {
+    try {
+      const response = await axios.post(`${API_URL}/session`, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      var result = await response.data;
+      cookie.set('access_token', result.access_token, { path: '/' });
+      getUser();
+      return await response.data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
