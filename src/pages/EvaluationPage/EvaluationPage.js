a3ff69f3b9d0e9b44e139eaab352edb1da34120b
Tim Bleicher <tim.bleicher@sap.com>
2023-07-17 11:39:37 +0200

    Waiting for PdfFile and Model Results

--- src/pages/EvaluationPage/EvaluationPage.js
+++ src/pages/EvaluationPage/EvaluationPage.js
@@ -4,7 +4,7 @@ import { bgColors } from '../../App';
 import { Container, Spinner } from 'react-bootstrap';
 import Header from './components/Header';
 import EvaluationComp from './components/EvaluationComp';
-import { getResults, getResultPdf } from '../../api/api'; // Hier die richtige Import-Anweisung hinzufügen
+import { getResults, getResultPdf } from '../../api/api';
 import './EvaluationPage.css';
 
 const EvaluationPage = () => {
@@ -14,43 +14,50 @@ const EvaluationPage = () => {
     const [results, setResults] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    // Funktion zum Laden der PDF-Datei
-    const loadPdfFile = async () => {
-        try {
-            const loadedPdf = await getResultPdf(documentIds[0]); // Wir gehen davon aus, dass es nur eine PDF-Datei gibt, daher verwenden wir das erste Element im documentIds-Array.
-            setPdfFile(loadedPdf);
-        } catch (error) {
-            console.error('Error loading PDF file:', error);
-        }
-    };
+    const loadData = async () => {
 
-    // Funktion zum Laden der Ergebnisse der Modelle
-    const loadResults = async () => {
-        const promises = documentIds.map(async (documentId) => {
-            const loadedResults = await getResults(documentId);
-            return loadedResults;
-        });
-        const loadedResults = await Promise.all(promises);
-        const allNotPending = loadedResults.every((result) => result.model_result !== "PENDING");
-        if (allNotPending) {
-            setResults(loadedResults);
-            setLoading(false);
-        } else {
-            setTimeout(loadResults, 5000);
-        }
+        const loadPdf = async () => {
+            try {
+                const loadedPdf = await getResultPdf(documentIds[0]);
+                if (loadedPdf.model_result === "PENDING") {
+                    await new Promise(resolve => setTimeout(resolve, 2500)); // Kurze Wartezeit von 2,5 Sekunden
+                    await loadPdf(); // Wenn die PDF-Datei noch im PENDING-Status ist, erneut abrufen
+                } else {
+                    setPdfFile(loadedPdf);
+                }
+            } catch (error) {
+                console.error('Error loading PDF file:', error);
+            }
+        };
+
+        const loadResults = async () => {
+            const promises = documentIds.map(async (documentId) => {
+                const loadedResults = await getResults(documentId);
+                return loadedResults;
+            });
+            const loadedResults = await Promise.all(promises);
+            const allNotPending = loadedResults.every((result) => result.model_result !== "PENDING");
+            if (allNotPending) {
+                setResults(loadedResults);
+            } else {
+                await new Promise(resolve => setTimeout(resolve, 2500)); // Kurze Wartezeit von 2,5 Sekunden
+                await loadResults(); // Wenn noch Ergebnisse im PENDING-Status sind, erneut abrufen
+            }
+        };
+
+        await Promise.all([loadPdf(), loadResults()]);
+        setLoading(false); // Sobald beide Funktionen abgeschlossen sind, setLoading(false) aufrufen
     };
 
     useEffect(() => {
-        // Laden sowohl der PDF-Datei als auch der Ergebnisse der Modelle beim ersten Rendern der Komponente
-        loadPdfFile();
-        loadResults();
+        loadData();
     }, [documentIds]);
 
     return (
-        <Container style={{maxWidth: "100%", padding: 0, height: "100%", color: "white"}}>
+        <Container style={{ maxWidth: "100%", padding: 0, height: "100%", color: "white" }}>
             {loading ? (
                 <div className="overlay">
-                    <p className="overlay-text" style={{marginTop: 15, marginRight: 10}}>Generating Questions, please wait</p>
+                    <p className="overlay-text" style={{ marginTop: 15, marginRight: 10 }}>Generating Questions, please wait</p>
                     <Spinner animation="border" variant="primary" />
                 </div>
             ) : (
